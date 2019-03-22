fb7078b15be0ed652fccc71cbc88e00513b52b4a
Watinguyaz <watimakhanu@gmail.com>
2019-03-22 19:30:50 +0300

    all models set

--- hood/models.py
+++ hood/models.py
@@ -3,20 +3,37 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt
 # Create your models here.
-class NeighbourHood (models.Model):
-    pass
+class Location(models.Model):
+    city = models.CharField(max_length=30)
+    hood = models.CharField(max_length=30)
 
-
+    def __str__(self):
+        return self.hood
 
 class User(models.Model):
-    pass
+    name = models.CharField(max_length=30)
+    id = models.IntegerField()
+    neighborhood_id=models.ForeignKey(NeighbourHood,blank=True, on_delete=models.CASCADE,related_name='neighborhood',null=True)
+    email address = models.EmailField(max_length=70,blank=True)
 
+    def __str__(self):
+        return self.name
+
+class NeighbourHood (models.Model):
+    neighbourhood_name = models.CharField(max_length=30)
+    location_id = models.ForeignKey(Location,blank=True, on_delete=models.CASCADE,related_name='location',null=True)
+    occupants_count = models.IntegerField()
+    user_id = models.ForeignKey(User,blank=True, on_delete=models.CASCADE,related_name='neighborhood',null=True)
+
+    def __str__(self):
+        return self.neighbourhood_name
 
 
 class Business(models.Model):
-    pass
+    business_name = models.CharField(max_length=30)
+    user = models.ForeignKey(User,blank=True, on_delete=models.CASCADE,related_name='neighborhood',null=True)
+    neighborhood_id = models.ForeignKey(NeighbourHood,blank=True, on_delete=models.CASCADE,related_name='neighborhood',null=True)
+    business_email = models.EmailField(max_length=70,blank=True)
 
-
-
-class Location(models.Model):
-    pass
+    def __str__(self):
+        return self.business_name
