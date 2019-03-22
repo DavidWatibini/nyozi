ed4c7e5a35f6b4f9fa6324fe06fb00884881e3b1
Watinguyaz <watimakhanu@gmail.com>
2019-03-22 13:25:42 +0300

    page routing to signup page set

--- hood/urls.py
+++ hood/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
-from django.conf.urls import url,include
 from django.conf.urls.static import static
+from django.conf.urls import url,include
 from . import views
 
 urlpatterns= [
-    url(r'',views.homm),
+    url(r'^$',views.homm),
+    url(r'signup/', views.signup, name="signup"),
 ]
--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def homm(request):
     return render(request,'home.html')
+
+def signup(request):
+    return render(request,'signup.html')
