2c9f503f4839106d8fd71aef718cf0abc3d4a8af
Watinguyaz <watimakhanu@gmail.com>
2019-03-22 14:25:11 +0300

    signup set and ready

--- hood/urls.py
+++ hood/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url,include
 from . import views
 
 urlpatterns= [
-    url(r'^$',views.homm),
+    url(r'^$',views.homm, name="home"),
     url(r'register/', views.register, name="register"),
+    url(r'accounts/', include('django.contrib.auth.urls')),
 ]
