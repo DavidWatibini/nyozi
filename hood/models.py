7643f14825019ce6563951c0009283f98c9c19e3
Watinguyaz <watimakhanu@gmail.com>
2019-03-22 15:42:43 +0300

    tests and models created, all tests for instance are failling

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,17 @@
+from django.shortcuts import render,redirect
 from django.db import models
-
+from django.contrib.auth.models import User
+import datetime as dt
 # Create your models here.
+class NeighbourHood (models.Model):
+    pass
+
+
+
+class User(models.Model):
+    pass
+
+
+
+class Business(models.Model):
+    pass
