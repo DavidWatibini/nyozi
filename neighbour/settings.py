a020066840f4424ae1efb61d9a841fa19974538d
Watinguyaz <watimakhanu@gmail.com>
2019-03-22 14:56:59 +0300

    take 2 deployment

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -149,7 +149,7 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static'),
-
+)
 LOGIN_REDIRECT_URL='home'
 
 django_heroku.settings(locals())
