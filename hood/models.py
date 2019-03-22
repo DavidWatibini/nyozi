94bf58ff605dab8011d1fec2bca277246a1964bd
Watinguyaz <watimakhanu@gmail.com>
2019-03-22 20:07:21 +0300

    changes on model file

--- hood/models.py
+++ hood/models.py
@@ -4,36 +4,33 @@ from django.contrib.auth.models import User
 import datetime as dt
 # Create your models here.
 class Location(models.Model):
-    city = models.CharField(max_length=30)
-    hood = models.CharField(max_length=30)
+    city = models.CharField(max_length=30, blank=True)
+    hood = models.CharField(max_length=30, blank=True)
 
     def __str__(self):
         return self.hood
 
-class User(models.Model):
-    name = models.CharField(max_length=30)
-    id = models.IntegerField()
-    neighborhood_id=models.ForeignKey(NeighbourHood,blank=True, on_delete=models.CASCADE,related_name='neighborhood',null=True)
-    email address = models.EmailField(max_length=70,blank=True)
-
-    def __str__(self):
-        return self.name
-
 class NeighbourHood (models.Model):
-    neighbourhood_name = models.CharField(max_length=30)
+    neighbourhood_name = models.CharField(max_length=30, blank=True)
     location_id = models.ForeignKey(Location,blank=True, on_delete=models.CASCADE,related_name='location',null=True)
-    occupants_count = models.IntegerField()
-    user_id = models.ForeignKey(User,blank=True, on_delete=models.CASCADE,related_name='neighborhood',null=True)
+    user_id = models.ForeignKey(User,blank=True, on_delete=models.CASCADE,related_name='user',null=True)
 
     def __str__(self):
         return self.neighbourhood_name
 
 
 class Business(models.Model):
-    business_name = models.CharField(max_length=30)
+    business_name = models.CharField(max_length=30, blank=True)
     user = models.ForeignKey(User,blank=True, on_delete=models.CASCADE,related_name='neighborhood',null=True)
     neighborhood_id = models.ForeignKey(NeighbourHood,blank=True, on_delete=models.CASCADE,related_name='neighborhood',null=True)
     business_email = models.EmailField(max_length=70,blank=True)
 
     def __str__(self):
         return self.business_name
+
+class User(models.Model):
+    name = models.CharField(max_length=30, blank=True)
+    email_address = models.EmailField(max_length=70,blank=True)
+
+    def __str__(self):
+        return self.name
