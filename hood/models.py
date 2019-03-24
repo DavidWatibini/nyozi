738db22d3f57b325eab3f634f30a936c5a5ca020
Watinguyaz <watimakhanu@gmail.com>
2019-03-24 18:01:56 +0300

    models edits

--- hood/models.py
+++ hood/models.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render,redirect
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 import datetime as dt
 # Create your models here.
+
+class UserProfile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
+    name = models.CharField(max_length=30, blank=True)
+    email_address = models.EmailField(max_length=70,blank=True)
+
+    def __str__(self):
+        return self.user
+    class Meta:
+        ordering= ['user']
+
+    def save_user(self):
+        self.save()
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender,instance, **kwargs):
+        instance.profile.save()
+
+
+
 class Location(models.Model):
     city = models.CharField(max_length=30, blank=True)
     hood = models.CharField(max_length=30, blank=True)
@@ -11,9 +38,9 @@ class Location(models.Model):
         return self.hood
 
 class NeighbourHood (models.Model):
+    user_id = models.ForeignKey(User,blank=True, on_delete=models.CASCADE,related_name='user',null=True)
     neighbourhood_name = models.CharField(max_length=30, blank=True)
     location_id = models.ForeignKey(Location,blank=True, on_delete=models.CASCADE,related_name='location',null=True)
-    user_id = models.ForeignKey(User,blank=True, on_delete=models.CASCADE,related_name='user',null=True)
 
     def __str__(self):
         return self.neighbourhood_name
@@ -27,10 +54,3 @@ class Business(models.Model):
 
     def __str__(self):
         return self.business_name
-
-class User(models.Model):
-    name = models.CharField(max_length=30, blank=True)
-    email_address = models.EmailField(max_length=70,blank=True)
-
-    def __str__(self):
-        return self.name
